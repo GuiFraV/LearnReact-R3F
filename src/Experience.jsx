2c26756a25a9a41344621cd405c73007a077ee40
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-05 19:18:30 +0200

    cubeAnimate

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,28 @@
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+
 export default function Experience()
 {
+
+    const cubeRef = useRef()
+
+    useFrame(() => 
+    {
+        cubeRef.current.rotation.y += 0.01
+    })
+
     return <>
         
             <mesh position-x={-2}  >
                 <sphereGeometry />
                 <meshBasicMaterial color='orange' />
             </mesh>
-            <mesh rotation-y={ Math.PI * 0.25 } position-x={2} scale={ 1.5 } >
+
+            <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.25 } position-x={2} scale={ 1.5 } >
                 <boxGeometry />
                 <meshBasicMaterial color='mediumpurple' />
             </mesh>
+
             <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
                 <planeGeometry />
                 <meshBasicMaterial color="greenyellow" />
