75392fb727af1d0b34d8fa01b84efb516900146f
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-05 19:05:09 +0200

    1st exercice

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,19 @@
 export default function Experience()
 {
     return <>
-        <mesh>
-            <sphereGeometry args={[1.5, 32, 32]}/>
-            <meshBasicMaterial args={[ { color: 'red', wireframe: true} ]}/>
-        </mesh>
+        
+            <mesh position-x={-3} scale={ 1.5 } >
+                <sphereGeometry scale={ 1.5 }/>
+                <meshBasicMaterial color='orange' />
+            </mesh>
+            <mesh position-x={3} scale={ 1.5 } >
+                <boxGeometry scale={ 1.5 }/>
+                <meshBasicMaterial color='purple' />
+            </mesh>
+            <mesh scale={ 10 } >
+                <planeBufferGeometry scale={ 1.5 }/>
+                <meshBasicMaterial color='green' />
+            </mesh>
+       
     </>
 }
