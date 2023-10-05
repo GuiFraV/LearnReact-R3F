7ff5a104076f55c6c59eaf944b31960b354b1f4d
GuiFraV <guillaume.franguiadakis@gmail.com>
2023-10-05 18:46:21 +0200

    add args

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,8 +2,8 @@ export default function Experience()
 {
     return <>
         <mesh>
-            <torusKnotGeometry />
-            <meshNormalMaterial />
+            <sphereGeometry args={[1.5, 32, 32]}/>
+            <meshBasicMaterial args={[ { color: 'red', wireframe: true} ]}/>
         </mesh>
     </>
 }
